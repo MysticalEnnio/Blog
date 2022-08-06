8721bee66fc0210b1182ef00f2ab9f58ff02fc12
MysticalEnnio <mystical.ennio@gmail.com>
2022-08-06 14:23:07 +0200

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,21 @@
 self.addEventListener("push", (e) => {
-  const data = e.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.message,
-    data: {
-      click_action: "https://blog.mystaredia.de/post?id=" + data.postId,
-    },
-    image:
-      "https://pixabay.com/vectors/bell-notification-communication-1096280/",
-    icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
-  });
+  setTimeout(() => {
+    const data = e.data.json();
+    self.registration.showNotification(data.title, {
+      body: data.message,
+      data: {
+        click_action: "https://blog.mystaredia.de/post?id=" + data.postId,
+      },
+      image:
+        "https://pixabay.com/vectors/bell-notification-communication-1096280/",
+      icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
+    });
+  }, 100);
 });
 
 self.addEventListener("notificationclick", function (event) {
   const data = e.data.json();
-  event.notification.close();
-  clients.openWindow("https://blog.mystaredia.de/post?id=" + data.postId);
+  event.waitUntil(
+    clients.openWindow("https://blog.mystaredia.de/post?id=" + data.postId)
+  );
 });
