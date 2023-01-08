93bb124369b52a3696b8ee6cf9da49cf386ab8cd
Ennio Marke <mystical.ennio@gmail.com>
2023-01-08 13:56:43 -0600

    loading issues and tailwind

--- public/resetPassword/js/index.js
+++ public/resetPassword/js/index.js
@@ -1,21 +1,3 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        pastel: {
-          1: "#01002E",
-          2: "#2F72BA",
-          3: "#3D9FDD",
-          4: "#EFB2EF",
-          5: "#D5BAC7",
-          6: "#DD74CF",
-          7: "#DD53B4",
-        },
-      },
-    },
-  },
-};
-
 const validateEmail = (email) => {
   return email.match(
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
--- public/signup/js/index.js
+++ public/signup/js/index.js
@@ -1,21 +1,3 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        pastel: {
-          1: "#01002E",
-          2: "#2F72BA",
-          3: "#3D9FDD",
-          4: "#EFB2EF",
-          5: "#D5BAC7",
-          6: "#DD74CF",
-          7: "#DD53B4",
-        },
-      },
-    },
-  },
-};
-
 console.log(window.location.origin);
 
 function togglePasswordVisibility() {
